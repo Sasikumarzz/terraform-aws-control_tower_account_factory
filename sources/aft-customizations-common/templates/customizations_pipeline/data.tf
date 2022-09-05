a2e8732b70ef949d6afc619c70f63e9c48e64d91
Sasikumarzz <99464570+Sasikumarzz@users.noreply.github.com>
2022-09-05 15:48:50 +0530

    Update data.tf
    sns topic data resource added

--- sources/aft-customizations-common/templates/customizations_pipeline/data.tf
+++ sources/aft-customizations-common/templates/customizations_pipeline/data.tf
@@ -5,6 +5,11 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
 
+data "aws_sns_topic" "aft-manual-approval-notification" {
+  name = "aft-manual-approval-notification"
+}
+
+
 data "aws_ssm_parameter" "aft_vcs_type" {
   name = "/aft/config/vcs/provider"
 }
