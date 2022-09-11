bba1d833cd3e9f6d00ff9f1bb12ce49a659185ca
Sasikumarzz <99464570+Sasikumarzz@users.noreply.github.com>
2022-09-11 12:29:53 +0530

    Update sns.tf

--- sources/aft-customizations-common/templates/customizations_pipeline/sns.tf
+++ sources/aft-customizations-common/templates/customizations_pipeline/sns.tf
@@ -1,9 +1,15 @@
+locals {
+  emails = ["sasikumar.ganesan@accenture.com"]
+}
+
 resource "aws_sns_topic" "aft-manual-approval-notification" {
   name = "aft-manual-approval-notification"
 }
 
 resource "aws_sns_topic_subscription" "sns-subscription" {
+  count     = length(local.emails)
   topic_arn = aws_sns_topic.aft-manual-approval-notification.arn
   protocol  = "email"
-  endpoint  = sasikumar.ganesan@accenture.com
+  endpoint  = local.emails[count.index]
+
 }
